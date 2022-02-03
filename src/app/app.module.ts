692ae692da7598f667b31e6f1f65d3e9f7107fcc
Hawer FORERO REY <hawerforero@gmail.com>
2022-02-03 16:35:35 -0500

    Ajuste rebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -103,7 +103,7 @@ export const API_GATEWAYS_PROVIDERS = [
     RecaptchaModule,
     RecaptchaFormsModule,
     NgbModule,
-    NgxPrintModule
+    NgxPrintModule,
     NgbModule,
     MatTableModule,
     MatButtonModule,
