012e6dec7165699818756ed55503fa171150e1a9
Hawer FORERO REY <hawerforero@gmail.com>
2022-02-16 13:53:56 -0500

    url api local

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,7 +7,7 @@ export const environment = {
   rest: {
   	endpoint: 'https://localhost:5001'
     //endpoint: 'http://10.1.18.124:5600'
-    // endpoint: 'https://localhost:44379',
+    //endpoint: 'https://localhost:44379',
   },
   recaptchaKey: '6LeBY9AcAAAAAD9fnMbTWQUSJQgsk2JBqRrS5wiF',
 };
