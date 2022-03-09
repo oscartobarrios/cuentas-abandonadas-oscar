03cc3bbadcd0e2c54435dd27660aec7e17290e33
Edgar Jaramillo Rodriguez <ejaramillor@icetex.gov.co>
2022-03-09 13:08:24 -0500

    desarrollo bug 14099 y 14100

--- src/app/presentation/app/pages/reportes/estado-cargues/estado-cargues.component.ts
+++ src/app/presentation/app/pages/reportes/estado-cargues/estado-cargues.component.ts
@@ -14,6 +14,7 @@ import { IEstadoCargue } from 'src/app/domain/models/reporte/iestadocargue';
 import { NotificationsService } from 'src/app/presentation/shared/services/notifications.service';
 import { MatPaginator } from '@angular/material/paginator';
 import { exit } from 'process';
+import { RETURN } from 'mat-table-exporter';
 
 @Component({
   selector: 'app-estado-cargues',
