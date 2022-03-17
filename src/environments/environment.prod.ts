528b82b4d50fd991e39244c7aacb6c84ede2058b
Hawer FORERO REY <hawerforero@gmail.com>
2022-03-17 16:08:04 -0500

    Ambiente pruebas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,11 +7,11 @@ export const environment = {
   rest: {
     //Producción
     //endpoint: 'https://aplicaciones.icetex.gov.co/WsCuentasAbandonadasAPI',
-    endpoint: 'http://10.1.18.124:5600',
-    //endpoint: 'https://appsitiospruebas.icetex.gov.co/AppCuentasAbandonadas',
+    //endpoint: 'http://10.1.18.124:5600',
+    endpoint: 'https://appsitiospruebas.icetex.gov.co/AppCuentasAbandonadas/ApiCuentasAbandonadas',
   },
   recaptchaKey: '6LeBY9AcAAAAAD9fnMbTWQUSJQgsk2JBqRrS5wiF',
-};
+};  
 
 /*
  * For easier debugging in development mode, you can import the following file
