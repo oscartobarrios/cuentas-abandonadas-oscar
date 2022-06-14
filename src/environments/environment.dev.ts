2a3d9fb317ff8d6a0cd4a8f97d384bf5768ca0d7
Johann Fernando Trigos Niño <jtrigos@icetex.gov.co>
2022-06-14 19:11:25 +0000

    Updated environment.dev.ts

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,7 +2,9 @@
 export const environment = {
   production: false,
   rest: {
-    endpoint: 'http://10.1.18.124:5600'
+    //endpoint: 'http://10.1.18.124:5600'
+    endpoint: 'https://aplicaciones.icetex.gov.co/WsCuentasAbandonadasAPI'
+    
   },
   recaptchaKey: '6LeBY9AcAAAAAD9fnMbTWQUSJQgsk2JBqRrS5wiF',
 };
