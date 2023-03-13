c4abce71f91a3c13ef6ee0e62a3025c16c0466fa
Edgar Jaramillo Rodriguez <ejaramillor@icetex.gov.co>
2023-03-13 11:22:56 -0500

    desarrollo tarea 4109

--- src/app/presentation/app/organisms/sub-menu-organism/sub-menu-organism.component.ts
+++ src/app/presentation/app/organisms/sub-menu-organism/sub-menu-organism.component.ts
@@ -59,7 +59,7 @@ export class SubMenuOrganismComponent implements OnInit {
     },{
       title: "Certificación de Saldos",
       url: "reportecertifiacionsaldos",
-      rols: [1,9]
+      rols: [1,9,4,5]
     },{
       title: "Consolidado",
       url: "consolidadoentidad",
